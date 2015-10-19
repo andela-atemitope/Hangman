d6c5ccc00e5495f88874046a0ebbd5d565fd75d2
andela.atemitope <amodut@gmail.com>
2015-10-19 16:14:43 +0100

    refactor attempt for circleci

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,18 +2,20 @@
 # //Coveralls.wear!
 require "simplecov"
 SimpleCov.start
-require "simplecov-shield"
+# require "simplecov-shield"
 require "fakefs/safe"
-require_relative "../lib/hangman/save_load.rb"
-require_relative "../lib/hangman/game_display.rb"
-require_relative "../lib/hangman/dictionary.rb"
-require_relative "../lib/hangman.rb"
-require_relative "../lib/hangman/win_lose.rb"
+require 'hangman'
+# require_relative "../lib/hangman/save_load.rb"
+# require_relative "../lib/hangman/game_display.rb"
+# require_relative "../lib/hangman/dictionary.rb"
+# require_relative "../lib/hangman.rb"
+# require_relative "../lib/hangman/win_lose.rb"
 
-
-class Hangman::Dictionary
-  def generate_word
-    generate_word = ["g", "e", "n", "e", "r", "a", "t", "e", "d"]
+module Hangman
+  class Dictionary
+    def generate_word
+      generate_word = ["g", "e", "n", "e", "r", "a", "t", "e", "d"]
+    end
   end
 end
-SimpleCov.formatter = SimpleCov::Formatter::ShieldFormatter
+# SimpleCov.formatter = SimpleCov::Formatter::ShieldFormatter
